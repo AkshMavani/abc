4cda4296fc3f06137a7930a98c2f1f8dae4e3561
AkshMavani <mavaniaksh3824@gmail.com>
2024-06-10 22:43:01 +0530

    blur...123

--- app/src/main/java/com/example/delete/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/delete/sqlite/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.delete.sqlite
 
 import android.annotation.SuppressLint
+import android.app.Dialog
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.view.Window
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.delete.R
 
@@ -15,12 +19,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
-//        val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-//        startActivity(myIntent)
+        val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+        startActivity(myIntent)
 
-        val intent=Intent(this,MyForegroundService::class.java)
-        startService(intent)
-
+//        val intent=Intent(this,MyForegroundService::class.java)
+//        startService(intent)
+        check()
+    }
+    fun check(){
+        val dialog= Dialog(this,R.style.CustomAlertDialog)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_location)
+        dialog.show()
     }
 
 
